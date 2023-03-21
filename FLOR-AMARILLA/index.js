9eb654ad4baea264ad48e3c674e30a757f866845
Mael0711 <101838890+Mael0711@users.noreply.github.com>
2023-03-21 17:55:34 -0500

    Delete FLOR-AMARILLA directory

--- FLOR-AMARILLA/index.js
+++
@@ -1,9 +0,0 @@
-const flower = document.querySelector('.flower');
-
-flower.addEventListener('click', () => {
-  for (let i = 1; i <= 5; i++) {
-    setTimeout(() => {
-      alert(`¡Alerta ${i}!`);
-    }, i * 1000);
-  }
-});
