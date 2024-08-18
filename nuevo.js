b9209d61def24cd375eca651f5599d077dc8f8bc
Abraham Ramos <abrahamramos.0711@gmail.com>
2024-08-18 17:04:09 -0500

    agregue un nuevo archivo js

---
+++ nuevo.js
@@ -0,0 +1,34 @@
+var pepito 
+
+pepito = "juanito"
+
+console.log(pepito)
+
+pepito = 2
+
+console.log(pepito)
+// esto es lo nuevo
+/*
+este es un comentario de varias lineas
+otra linea 
+otra linea
+*/
+
+const dato = "hola"
+
+console.log(dato)
+
+let dato2 = "chao1"
+
+console.log("este es el dato2",dato2)
+dato2 = 500
+
+console.log("este es el nuevo dato2",dato2)
+
+dato2 = 2+2
+
+console.log("el resultado",dato2)
+
+dato2 = "2+2"
+
+console.log(dato2)
