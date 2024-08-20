b41e7cbff79c2e207c6d98e234292d5447971d7d
Abraham Ramos <abrahamramos.0711@gmail.com>
2024-08-19 19:11:15 -0500

    se agrego un nuevo archivo js

---
+++ app.js
@@ -0,0 +1,18 @@
+const user = "abraham"
+
+let quantity = 14 
+
+let flag = true 
+
+// palabras reservadas
+/*
+number, float, double, swich, break, while, for, string, char
+*/
+
+console.log(user)
+
+console.log(quantity)
+
+console.log(flag)
+
+console.error(user)
